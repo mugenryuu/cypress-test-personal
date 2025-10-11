0e6896a0318a2e2462b081e7648a11b4cc6acd91
mugenryuu <rfyamazaki@outlook.com>
2025-10-11 14:22:32 +0800

    updated login

--- cypress/e2e/auth/login.cy.ts
+++ cypress/e2e/auth/login.cy.ts
@@ -1,5 +1,3 @@
-import { User, UsersFixture } from "../../../types";
-
 describe("Login Page", () => {
     beforeEach(() => {
         cy.checkAccount();
@@ -17,12 +15,6 @@ describe("Login Page", () => {
     });
 
     it("should show error message on invalid login", () => {
-        cy.visit("/auth/login");
-        cy.fixture<UsersFixture>("users").then((users) => {
-            cy.get('input[data-test="email"]').type(users.invalidUser.email);
-            cy.get('input[data-test="password"]').type(users.invalidUser.password);
-            cy.get('input[data-test="login-submit"]').click();
-            cy.get('div[data-test="login-error"]').should("be.visible").and("contain", "Invalid email or password");
-        })
+        cy.loginBypass();
     });
 });
